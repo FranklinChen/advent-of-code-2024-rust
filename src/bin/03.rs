65c451b785bd8b14cf0ed7ca150eefc01de3fe83
Franklin Chen <franklinchen@franklinchen.com>
2024-12-08 00:24:05 -0500

    Day 03, part 1.

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,7 +1,28 @@
 advent_of_code::solution!(3);
 
+use std::sync::LazyLock;
+use regex::Regex;
+
+static RE: LazyLock<Regex> = LazyLock::new(|| {
+    Regex::new(r"mul\((\d+),(\d+)\)").expect("Failed to compile regex")
+});
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    // Extract info from the entire input (not breaking up into lines):
+    // Find all "mul(X,Y)" where X and Y are 1-3 digit numbers.
+    // Use regex.
+    // Parse each instance into a tuple of (X, Y) as u32 each.
+    // Sum all of the multiplications.
+    // Return the sum.
+    let sum: u32 = RE
+        .captures_iter(input)
+        .map(|cap| {
+            let x: u32 = cap[1].parse().unwrap();
+            let y: u32 = cap[2].parse().unwrap();
+            x * y
+        })
+        .sum();
+    Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
