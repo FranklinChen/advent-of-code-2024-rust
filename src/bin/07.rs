7199d41045ddbd5edc541b1b98c11dcac1b152fb
Franklin Chen <franklinchen@franklinchen.com>
2024-12-09 01:10:27 -0500

    Day 07, part 2.

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -57,6 +57,63 @@ fn can_equal_target(target: u64, xs: &[u64]) -> bool {
     }
 }
 
+fn can_equal_target_2(target: u64, xs: &[u64]) -> bool {
+    // Base case.
+    if xs.is_empty() {
+        return target == 0;
+    }
+    if target == 0 {
+        // There are still xs left.
+        return false;
+    }
+
+    let last = xs.len() - 1;
+
+    // If there is only one number remaining, it must be the target.
+    if last == 0 {
+        return xs[0] == target;
+    }
+
+    let last_x = xs[last];
+
+    // See if * could be the final operation.
+    if target % last_x == 0 {
+        if can_equal_target_2(target / last_x, &xs[0..last]) {
+            return true;
+        }
+    }
+
+    // See if || could be the final operation, by checking
+    // in lockstep the final digits of last_x and target,
+    // trying to see if all of the digits of last_x are the final
+    // digits of target.
+    if target >= last_x {
+        let mut last_x_prefix = last_x;
+        let mut target_prefix = target;
+
+        // Compare digits from right to left.
+        let mut mismatch_found = false;
+        while last_x_prefix > 0 {
+            if last_x_prefix % 10 != target_prefix % 10 {
+                mismatch_found = true;
+                break;
+            }
+            last_x_prefix /= 10;
+            target_prefix /= 10;
+        }
+
+        if !mismatch_found {
+            if can_equal_target_2(target_prefix, &xs[0..last]) {
+                return true;
+            }
+        }
+    }
+
+    // Finally, see if addition works.
+    return target >= last_x && can_equal_target_2(target - last_x, &xs[0..last])
+}
+
+
 pub fn part_one(input: &str) -> Option<u64> {
     // Return the sum of all targets that can be formed from the xs
     // using left-to-right evaluation with + and *.
@@ -76,7 +133,22 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    // Return the sum of all targets that can be formed from the xs
+    // using left-to-right evaluation with + and * and || which is
+    // concatenation of digits as strings.
+    let lines = parse_input(input);
+    Some(
+        lines
+            .iter()
+            .filter_map(|line| {
+                if can_equal_target_2(line.target, &line.xs) {
+                    Some(line.target)
+                } else {
+                    None
+                }
+            })
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -92,6 +164,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11387));
     }
 }
